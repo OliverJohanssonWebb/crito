82bdd8213de3710297c9f54fc3b05afa2b9b0368
Oliver Johansson <98oliver.johansson@gmail.com>
2023-11-18 13:45:13 +0100

    Final touch

--- src/Components/Showcasesection.js
+++ src/Components/Showcasesection.js
@@ -9,7 +9,7 @@ const Showcasesection = () => {
             <div className="content">
                 <h1>We Provide The Best Business Solutions</h1>
                 <p>Establish your vision and value proposition and turn them into testable prototypes.</p>
-                <a className="btn-yellow" href="consulting.html">Get Consulting<i className="fa-solid fa-arrow-turn-up"></i></a>
+                <a className="btn-yellow" href="/Contacts">Get Consulting<i className="fa-solid fa-arrow-turn-up"></i></a>
                 <a className="btn-transparent" href="Services.html">Learn More<i className="fa-solid fa-arrow-turn-up"></i></a>
             </div>
             <img src={showcaseImage} alt="" />
